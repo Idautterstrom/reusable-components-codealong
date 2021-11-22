026b1683761e84ab5a449b55d0ac3d5aa6d8afea
Idautterstrom <ida.utterstrom@hotmail.com>
2021-11-22 11:49:17 +0100

    added code and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components/macro";
+import { Card } from "lib/Card";
+
+const ThinnerCard = styled(Card)`
+  width: 400px;
+`;
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <ThinnerCard
+        coverImage="https://www.unsplash.it/500/300"
+        thumbnailUrl="https://www.unsplash.it/100/100"
+        title="This is the title"
+        secondaryText="Secondary title"
+      />
+
+      <Card title="Only a title" secondaryText="Secondary text" />
     </div>
-  )
-}
+  );
+};
